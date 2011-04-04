d76d1ec79ae1ad3a48c38a89315e237e0eff0192
Bilal Khan <bilal.khan@me.com>
2011-04-04 16:35:31 +0000

    Updated Mac build instructions to make audio lab an optional install, specified exactly which bits of the Xcode install are actually necessary.

--- doc/Developer_Manual/macinstall_text.php
+++ doc/Developer_Manual/macinstall_text.php
@@ -9,6 +9,8 @@ The method outlined below provides greater speed and compatibility than the EPD,
 <p>Free Option: <a href="http://developer.apple.com/xcode/">[Apple Developer Website]</a> install Xcode 3.2.6 from the link in the lower bottom right of the page.<br />
 Paid Option: <a href="http://itunes.apple.com/app/xcode/id422352214?mt=12">[Mac AppStore]</a> install Xcode 4.*</p>
 
+<p>Only the Xcode Essentials & UNIX development tools are required for Topographica, the remaining Xcode install options can safely be omitted.</p>
+
 <p><a href="http://xquartz.macosforge.org/trac/wiki">[XQuartz Website]</a> install XQuartz<br />
 <a href="http://www.macports.org/">[MacPorts Website]</a> install MacPorts</p>
 
@@ -19,7 +21,7 @@ Run the following in a shell to install everything needed for topographica:
 
 <blockquote><code>
 sudo port selfupdate<br />
-sudo port install tcl +threads tk python26 py26-numpy py26-pil py26-matplotlib py26-scipy py26-ipython py26-scikits-audiolab gmp python_select<br />
+sudo port install tcl +threads tk python26 py26-numpy py26-pil py26-matplotlib py26-scipy py26-ipython gmp python_select<br />
 <br />
 sudo python_select python26<br />
 sudo ln -s /opt/local/bin/ipython-2.6 /opt/local/bin/ipython<br />
@@ -41,6 +43,13 @@ svn co $TOPOROOT/trunk/topographica topographica<br />
 cd topographica<br />
 svn update<br />
 make topographica-other-python<br />
+</code></blockquote>
+
+<h3>Optional Installs</h3>
+<p>The audiolab package must be installed to use any of Topographica's auditory system models.</p>
+
+<blockquote><code>
+sudo port install py26-scikits-audiolab 
 </code></blockquote>
 
 <h3>Notes/Issues</h3>
