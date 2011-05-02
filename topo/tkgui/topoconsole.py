4d73e7190b5daaebc83cd4079764c35e5f87755d
Chris Ball <C.E.Ball-2@sms.ed.ac.uk>
2011-05-02 20:53:41 +0000

    Reverted recent change. Full tracebacks should not be printed to the console.

--- topo/tkgui/topoconsole.py
+++ topo/tkgui/topoconsole.py
@@ -268,10 +268,11 @@ def _tkinter_report_exception(widget):
     else:
         topo.guimain.messageBar.error('%s'%msg)
     
-    # BK-NOTE: Uncommented the import traceback below to get 
-    # full error messages/traces printed to the console.
+    # CEBALERT: need to implement gui debugging mode to have full
+    # traceback show up in a window.
+    # http://sourceforge.net/tracker/?func=detail&atid=470932&aid=3292417&group_id=53602
     param.Parameterized().warning(msg)
-    import traceback; traceback.print_exc()
+    #import traceback; traceback.print_exc()
 
 
 import Tkinter
