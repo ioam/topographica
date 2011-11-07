f8ee3f309d621b9090e4cdb9e1ae947bd171fb26
James A. Bednar <jbednar@inf.ed.ac.uk>
2011-11-07 13:23:44 +0000

    Removed outdated import

--- topo/learningfn/projfn.py
+++ topo/learningfn/projfn.py
@@ -22,7 +22,7 @@ from topo.base.functionfamily import Hebbian,LearningFn
 # Imported here so that all ProjectionLearningFns will be in the same package
 from topo.base.cf import CFPLF_Identity,CFPLF_Plugin
 
-from basic import BCMFixed
+#from basic import BCMFixed
 
 
 class CFPLF_EuclideanHebbian(CFPLearningFn):
