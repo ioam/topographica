15557e2d56a9cca184407f21a94daf3f2fc1f49a
jbednar <jbednar@inf>
2012-11-23 01:57:48 +0000

    Updated for release 0.9.8

--- doc/Home/news_text.php
+++ doc/Home/news_text.php
@@ -5,6 +5,14 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
 <i>
 <b>Recent news:</b>
 
+<p><b>23 Nov 2012:</b> Version 0.9.8
+  <A target="_top" href="../News/index.html#23-nov-2012">released</A>.
+  Screenshots from previous releases: 
+  <A target="_top" href="../images/080903_plotting1_fedora.png">plotting 1</A> (Linux),
+  <A target="_top" href="../images/080903_plotting2_mac.png">plotting 2</A> (Mac), 
+  <A target="_top" href="../images/080903_docking_fedora.png">docking</A> (Linux),
+  <A target="_top" href="../images/080903_modeleditor_fedora.png">model editor</A> (Linux).
+  
 <p><b>3 Oct 2012:</b> Topographica development moved from
   <a target="_top" href="http://sourceforge.net/projects/topographica">SourceForge
   Subversion</a> to <a target="_top" href="http://github.com/ioam/topographica">
@@ -15,18 +23,6 @@ print '<table width="100%" cellpadding="5"><tr><td bgcolor="'.$banner_bg_color.'
   <A href="http://ioam.github.com/paramtk/">ParamTk</A>, and
   <A href="http://ioam.github.com/imagen/">ImaGen</A>
   spin-out projects from Topographica.
-
-<p><b>30 Nov 2011:</b> Unofficial release available in SVN; see the <A
-  target="_top" href="../News/index.html#30-nov-2011">News</A> page for a list of recent
-  features.
-  
-<p><b>18 July 2010:</b> Version 0.9.7
-  <A target="_top" href="../Downloads/index.html">released</A>.
-  Screenshots from previous releases: 
-  <A target="_top" href="../images/080903_plotting1_fedora.png">plotting 1</A> (Linux),
-  <A target="_top" href="../images/080903_plotting2_mac.png">plotting 2</A> (Mac), 
-  <A target="_top" href="../images/080903_docking_fedora.png">docking</A> (Linux),
-  <A target="_top" href="../images/080903_modeleditor_fedora.png">model editor</A> (Linux).
 </i>
 
 </blockquote>
