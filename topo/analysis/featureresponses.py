9d55c278e363551438c1fe8052077a5856f01f71
jlstevens <jlstevens[at]ed.ac.uk>
2013-05-21 01:17:13 +0100

    Forcing update_activity in measure_response to ensure update regardless of topo.sim.time()

--- topo/analysis/featureresponses.py
+++ topo/analysis/featureresponses.py
@@ -1180,7 +1180,7 @@ class measure_response(PatternPresentingCommand):
         if not p.overwrite_previous:
             restore_input_generators()
 
-        update_activity(p.sheet_views_prefix)
+        update_activity(p.sheet_views_prefix, force=True)
 
 class MeasureResponseCommand(PatternPresentingCommand):
     """Parameterized command for presenting input patterns and measuring responses."""
