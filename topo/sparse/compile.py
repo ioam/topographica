7ed1c635bb820212176d11f6202e06c01beb1863
Yury V. Zaytsev <yury@shurup.com>
2013-10-25 17:46:42 +0200

    Run the setup script in the sandbox, so that it doesn't complain about unknown args when launched through nose
    Signed-off-by: Yury V. Zaytsev <yury@shurup.com>

---
+++ topo/sparse/compile.py
@@ -0,0 +1,18 @@
+import os
+import numpy
+
+(basepath, _) = os.path.split(os.path.abspath(__file__))
+(rootpath, _) = os.path.split(os.path.split(basepath)[0])
+
+from Cython.Distutils import build_ext
+from distutils.core import setup
+from distutils.extension import Extension
+
+ext_modules = [Extension("sparse", [basepath + "/sparse.pyx"],
+                         language="c++", extra_compile_args = ["-w","-O2","-fopenmp","-DNDEBUG","-msse2"],
+                         extra_link_args=['-lgomp'])]
+
+setup_args = ['--quiet','build_ext','--build-lib',basepath]
+setup(name = "Sparse CF Matrix",  ext_modules = ext_modules,
+      include_dirs=[rootpath+'/external/',numpy.get_include()],
+      cmdclass = {'build_ext':build_ext}, script_args = setup_args)
