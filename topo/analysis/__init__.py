ac485de923dee83476debb92b607fcc053f2a051
philippjfr <P.Rudiger@ed.ac.uk>
2013-12-13 19:02:52 +0000

    Moved FeatureMapper interface definitions into analysis/__init__.py

--- topo/analysis/__init__.py
+++ topo/analysis/__init__.py
@@ -2,6 +2,22 @@
 Analysis tools for Topographica, other than plotting tools.
 """
 
+from topo.analysis.featureresponses import FeatureResponses, FeatureCurves,\
+    FeatureMaps, ReverseCorrelation, MeasureResponseCommand, pattern_response,\
+    topo_metadata_fn, store_rfs, store_maps, store_curves, store_activity, \
+    get_feature_preference
+from fmapper.command import measure_activity
+
+FeatureResponses.metadata_fns = [topo_metadata_fn]
+FeatureResponses.pattern_response_fn = pattern_response.instance()
+FeatureMaps.measurement_storage_hook = store_maps
+FeatureCurves.measurement_storage_hook = store_curves
+ReverseCorrelation.measurement_storage_hook = store_rfs
+measure_activity.measurement_storage_hook = store_activity
+
+MeasureResponseCommand.preference_lookup_fn = get_feature_preference
+MeasureResponseCommand.pattern_response_fn = pattern_response.instance()
+
 
 # Automatically discover all .py files in this directory.
 import os,fnmatch
