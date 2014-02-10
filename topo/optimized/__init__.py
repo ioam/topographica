598224b2dd4f40a9686be63cf3c809cd128d99fc
philippjfr <P.Rudiger@ed.ac.uk>
2014-02-10 16:51:06 +0000

    Added pyflakes ignore statements to optimized module

--- topo/optimized/__init__.py
+++ topo/optimized/__init__.py
@@ -10,8 +10,8 @@ try:
     # Run the setup script in the sandbox, so that it doesn't complain about unknown args when launched through nose
     run_setup(basepath + "/compile.py")
 
-    from optimized import *
+    from optimized import * # pyflakes:ignore (API import)
 except ImportError:
     print "WARNING: Install distutils and Cython to build optimized component, " \
           "falling back to unoptimized components."
-    from unoptimized import *
+    from unoptimized import * # pyflakes:ignore (API import)
