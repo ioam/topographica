09e07dfce04f5809cd2c30263695fe4c5c4abe04
jlstevens <jlstevens[at]ed.ac.uk>
2014-02-11 20:01:06 +0000

    Added example of how to set default imagen video format from profile

--- platform/ipython/profile_topo/ipython_config.py
+++ platform/ipython/profile_topo/ipython_config.py
@@ -8,3 +8,8 @@ project_dir = os.path.join(profile_dir, '..', '..', '..')
 sys.path = [os.path.abspath(project_dir)] + sys.path
 
 import external
+
+# Example of how to change the default imagen video format via profile
+
+# from imagen import ipython
+# ipython.VIDEO_FORMAT = 'gif'
