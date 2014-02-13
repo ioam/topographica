e5f6b38c047ec7aa1dba045ebe7da24f6a37d2e8
jlstevens <jlstevens[at]ed.ac.uk>
2014-02-13 21:17:56 +0000

    Adding the default_output_path to search_paths in the topo profile

--- platform/ipython/profile_topo/ipython_config.py
+++ platform/ipython/profile_topo/ipython_config.py
@@ -8,15 +8,19 @@ project_dir = os.path.join(profile_dir, '..', '..', '..')
 sys.path = [os.path.abspath(project_dir)] + sys.path
 
 import external
+import param
+
+from topo.misc.commandline import default_output_path
+param.resolve_path.search_paths.append(default_output_path())
 
 # To customize your personal settings, (e.g default settings of the
-# Topographica IPython extension), add a python script file to the
-# 'startup' folder. For instance, you could add a file 'settings.py'
-# with the following contents:
+# Topographica or Imagen IPython extensions), add a python script file
+# to the 'startup' folder. For instance, you could introduce a startup
+# file 'settings.py' with the following contents:
 
 # import dataviews
 # dataviews.ipython.VIDEO_FORMAT = 'gif'
 # dataviews.ipython.WARN_MISFORMATTED_DOCSTRINGS = True
 
-# For more information, consult the README file in the 'startup'
-# directory.
+# For more information, please consult the README file in the
+# 'startup' directory.
