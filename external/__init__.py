399c14d91d00d9703a2442872f8887385f84f3ea
philippjfr <P.Rudiger@ed.ac.uk>
2014-05-08 16:48:16 +0100

    Added exception if submodules have not been initialized

--- external/__init__.py
+++ external/__init__.py
@@ -5,6 +5,14 @@ submodules = ['param', 'paramtk', 'imagen', 'featuremapper', 'lancet', 'dataview
 def sys_paths():
     cwd = os.path.abspath(os.path.split(__file__)[0])
     topo_path = os.path.abspath(os.path.join(cwd, '..'))
-    return [os.path.join(cwd, s) for s in submodules] + [topo_path]
+    module_dirs = [os.path.join(cwd, s) for s in submodules]
+    for submodule, module_dir in zip(submodules, module_dirs):
+        if not os.path.isdir(module_dir) or not os.listdir(module_dir):
+            raise ImportError('{submodule} submodule files not found in {module_dir}. '
+                              'Please run `git submodule update --init` before '
+                              'launching Topographica.'.format(submodule=submodule,
+                                                               module_dir=module_dir))
+
+    return module_dirs + [topo_path]
 
 sys.path = sys_paths() + sys.path
