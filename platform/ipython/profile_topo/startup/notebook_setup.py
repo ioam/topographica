ffad679701b8aa64008aaf3ee53380df1929eeca
jlstevens <jlstevens@ed.ac.uk>
2014-05-31 19:23:01 +0100

    Disabling measurement storage hooks in IPython Notebook

--- platform/ipython/profile_topo/startup/notebook_setup.py
+++ platform/ipython/profile_topo/startup/notebook_setup.py
@@ -8,6 +8,17 @@ import sys
 from topo.command import pylabplot
 pylabplot.PylabPlotCommand.display_window = False
 
+from topo.analysis.command import measure_cog
+from topo.analysis.featureresponses import FeatureCurves, FeatureMaps, ReverseCorrelation
+from featuremapper.command import measure_response
+
+FeatureMaps.measurement_storage_hook = lambda x: None
+FeatureCurves.measurement_storage_hook = lambda x: None
+ReverseCorrelation.measurement_storage_hook = lambda x: None
+measure_response.measurement_storage_hook = lambda x: None
+measure_cog.measurement_storage_hook = lambda x: None
+
+
 # Create new logger to ensure param logging shows in notebooks
 iplogger = logging.getLogger('ip-param')
 iplogger.addHandler(logging.StreamHandler(stream=sys.stderr))
