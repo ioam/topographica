7458d1d4e4dba79dd742d175e142991aa573e2ff
jlstevens <jlstevens@ed.ac.uk>
2014-06-01 16:57:05 +0100

    Added additional check to external/__init__.py for topo git repository

--- external/__init__.py
+++ external/__init__.py
@@ -5,6 +5,8 @@ submodules = ['param', 'paramtk', 'imagen', 'featuremapper', 'lancet', 'dataview
 def sys_paths():
     cwd = os.path.abspath(os.path.split(__file__)[0])
     topo_path = os.path.abspath(os.path.join(cwd, '..'))
+    if not os.path.isdir(os.path.join(topo_path, '.git')):
+        return []
     module_dirs = [os.path.join(cwd, s) for s in submodules]
     for submodule, module_dir in zip(submodules, module_dirs):
         if not os.path.isdir(module_dir) or not os.listdir(module_dir):
