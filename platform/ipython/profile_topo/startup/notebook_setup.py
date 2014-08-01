2e3d3ef25cf5b130428a6100f9e773c3689d5967
philippjfr <P.Rudiger@ed.ac.uk>
2014-08-01 18:10:56 +0100

    Reenabled global measurement AttrTree in IPython Notebooks

--- platform/ipython/profile_topo/startup/notebook_setup.py
+++ platform/ipython/profile_topo/startup/notebook_setup.py
@@ -8,18 +8,6 @@ import sys
 from topo.command import pylabplot
 pylabplot.PylabPlotCommand.display_window = False
 
-from topo.analysis import empty_storage_hook
-from topo.analysis.command import measure_cog
-from topo.analysis.featureresponses import FeatureCurves, FeatureMaps, ReverseCorrelation
-from featuremapper.command import measure_response
-
-FeatureMaps.measurement_storage_hook = empty_storage_hook
-FeatureCurves.measurement_storage_hook = empty_storage_hook
-ReverseCorrelation.measurement_storage_hook = empty_storage_hook
-measure_response.measurement_storage_hook = empty_storage_hook
-measure_cog.measurement_storage_hook = empty_storage_hook
-
-
 # Create new logger to ensure param logging shows in notebooks
 iplogger = logging.getLogger('ip-param')
 iplogger.addHandler(logging.StreamHandler(stream=sys.stderr))
