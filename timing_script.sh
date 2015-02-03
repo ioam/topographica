53ec42464ab736de0cd33f22876269979baf985b
Ignotas <tasignotas@gmail.com>
2015-02-03 20:01:40 +0000

    Timing script, called by iPython notebook

--- timing_script.sh
+++ timing_script.sh
@@ -6,38 +6,20 @@ time ./topographica -c quit
 
 echo ----------------------------------
 
-echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density + running 0 iterations:
+echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density + running 0 iterations \(gpu=$3\):
 
-time ./topographica -p cortex_density=$1 -p gpu=False examples/gcal_oo_or.ty -c "topo.sim.run(0)"
-
-echo ----------------------------------
-
-echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density + running 1 iteration:
-
-time ./topographica -p cortex_density=$1 -p gpu=False examples/gcal_oo_or.ty -c "topo.sim.run(1)"
+time ./topographica -p cortex_density=$1 -p gpu=$3 examples/gcal_oo_or.ty -c "topo.sim.run(0)"
 
 echo ----------------------------------
 
-echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density + running $2 iterations:
+echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density + running 1 iteration \(gpu=$3\):
 
-time ./topographica -p cortex_density=$1 -p gpu=False examples/gcal_oo_or.ty -c "topo.sim.run($2)"
-
-echo ----------------------------------
-
-echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density and GPU + running 0 iterations:
-
-time ./topographica -p cortex_density=$1 -p gpu=True examples/gcal_oo_or.ty -c "topo.sim.run(0)"
+time ./topographica -p cortex_density=$1 -p gpu=$3 examples/gcal_oo_or.ty -c "topo.sim.run(1)"
 
 echo ----------------------------------
 
-echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density and GPU + running 1 iteration:
+echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density + running $2 iterations \(gpu=$3\):
 
-time ./topographica -p cortex_density=$1 -p gpu=True examples/gcal_oo_or.ty -c "topo.sim.run(1)"
-
-echo ----------------------------------
-
-echo Loading Topographica + setting up gcal_oo_or.ty using $1 cortex density and GPU + running $2 iterations:
-
-time ./topographica -p cortex_density=$1 -p gpu=True examples/gcal_oo_or.ty -c "topo.sim.run($2)"
+time ./topographica -p cortex_density=$1 -p gpu=$3 examples/gcal_oo_or.ty -c "topo.sim.run($2)"
 
 echo ----------------------------------
