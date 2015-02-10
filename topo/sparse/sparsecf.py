Fixed a mistake in calculating the normalisation values

topo/sparse/sparsecf.py
@@ -362,13 +362,11 @@ def compute_sparse_gpu_joint_norm_totals(projlist,active_units_mask=True):
     joint_sum = gpuarray.zeros((projlist[0].weights_gpu.shape[0], ), np.float32)
     for p in projlist:
         if not p.has_norm_total:
-            joint_sum = p.weights_gpu.mv(p.norm_ones_gpu, y=p.zeros_gpu, autosync=False)
-            p.has_norm_total=True
-        else:
-            joint_sum += p.norm_total_gpu
+            p.norm_total_gpu = p.weights_gpu.mv(p.norm_ones_gpu, y=p.zeros_gpu, autosync=False)
+            p.has_norm_total = True
+        joint_sum += p.norm_total_gpu
     for p in projlist:
         p.norm_total_gpu = joint_sum.copy()
-
 
 
 def CFPOF_DivisiveNormalizeL1_Sparse(projection):
@@ -418,6 +416,10 @@ def CFPLF_Hebbian_Sparse_GPU(projection):
 
     # Computing Hebbian learning weights:
     projection.hebbian_kernel(single_conn_lr, projection.nzrows_gpu, projection.nzcols_gpu, src_activity_gpu, dest_activity_gpu, projection.weights_gpu.Val, range=slice(0, projection.nzcount, 1))
+
+    # Normalisation values:
+    projection.norm_total_gpu = projection.weights_gpu.mv(projection.norm_ones_gpu, y=projection.zeros_gpu, autosync=False)
+    projection.has_norm_total = True
 
 
 def CFPLF_Hebbian_Sparse_opt(projection):