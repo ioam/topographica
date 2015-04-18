f4be2aa7783778862532cf1941c1abf11bd07d18
Ignotas <tasignotas@gmail.com>
2015-04-18 22:19:16 +0100

    Moved timing_script into test folder and updated the model that is used for benchmarking

---
+++ topo/tests/timing_script.sh
@@ -0,0 +1,25 @@
+#!/bin/tcsh
+
+echo HOST:"\t"$HOST
+
+echo PARAMS:"\t"$1"\t"$2"\t"$3
+
+echo VERSION:"\t"`git describe --tags`
+
+echo Loading Topographica ant quitting:
+
+time ../../topographica -c quit
+
+echo ----------------------------------
+
+echo Loading Topographica + setting up gcal_sparse.ty using $1 cortex density + running 0 iterations \(gpu=$3\):
+
+time ../../topographica -p cortex_density=$1 -p gpu=$3 ../../examples/gcal_sparse.ty -c "topo.sim.run(0)"
+
+echo ----------------------------------
+
+echo Loading Topographica + setting up gcal_sparse.ty using $1 cortex density + running $2 iterations \(gpu=$3\):
+
+time ../../topographica -p cortex_density=$1 -p gpu=$3 ../../examples/gcal_sparse.ty -c "topo.sim.run($2)"
+
+echo ----------------------------------
