6a293c368c9fa46eece727ccdbcb7a9f2854fece
Ignotas <tasignotas@gmail.com>
2015-04-21 17:23:40 +0100

    Improved the timing script by adding an extra parameter for the model file to be used for benchmarking

--- topo/tests/timing_script.sh
+++ topo/tests/timing_script.sh
@@ -2,7 +2,7 @@
 
 echo HOST:"\t"$HOST
 
-echo PARAMS:"\t"$1"\t"$2"\t"$3
+echo PARAMS:"\t"$1"\t"$2"\t"$3"\t"$4
 
 echo VERSION:"\t"`git describe --tags`
 
@@ -12,14 +12,14 @@ time ../../topographica -c quit
 
 echo ----------------------------------
 
-echo Loading Topographica + setting up gcal_sparse.ty using $1 cortex density + running 0 iterations \(gpu=$3\):
+echo Loading Topographica + setting up $1 using $2 cortex density + running 0 iterations \(gpu=$4\):
 
-time ../../topographica -p cortex_density=$1 -p gpu=$3 ../../examples/gcal_sparse.ty -c "topo.sim.run(0)"
+time ../../topographica -p cortex_density=$2 -p gpu=$4 $1 -c "topo.sim.run(0)"
 
 echo ----------------------------------
 
-echo Loading Topographica + setting up gcal_sparse.ty using $1 cortex density + running $2 iterations \(gpu=$3\):
+echo Loading Topographica + setting up $1 using $2 cortex density + running $3 iterations \(gpu=$4\):
 
-time ../../topographica -p cortex_density=$1 -p gpu=$3 ../../examples/gcal_sparse.ty -c "topo.sim.run($2)"
+time ../../topographica -p cortex_density=$2 -p gpu=$4 $1 -c "topo.sim.run($3)"
 
 echo ----------------------------------
